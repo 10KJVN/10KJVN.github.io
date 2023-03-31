215a5cc800bd33797530568e5a7e51825aefef3b
10KJVN <Jahva_game11@hotmail.com>
2023-03-31 20:43:31 +0200

    Revert "Revert "Added Slideshow (w.i.p.)""
    This reverts commit 42d9e3da864024c6ef3b79f9e9f89a24e1bf37a6.

--- website/js/main.js
+++ website/js/main.js
@@ -1,3 +1,28 @@
-const button = document.getElementById("js--menu");
-const navigation = document.getElementById("js--nav");
-const body = document.getElementById("js--body");
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
