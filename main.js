527ea3bda11a9b3569892913fab4d15d697b2c92
10KJVN <Jahva_game11@hotmail.com>
2023-10-17 15:03:47 +0200

    Fixed home, modified CSS.
    the most stable version of my site until now.
    
    Need to fix Navbar, Project Pages, Slider

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
-document.getElementById("defaultOpen").click();
+// Add a click event listener to the tabs
+const tabs = document.querySelectorAll('.tab');
+const features = document.querySelectorAll('.feature');
 
-function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
-  
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-  
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        // Remove 'active' class from all tabs and features
+        tabs.forEach(t => t.classList.remove('active'));
+        features.forEach(f => f.classList.remove('active'));
+
+        // Add 'active' class to the clicked tab and corresponding feature
+        const tabNumber = tab.getAttribute('data-tab');
+        const feature = document.getElementById(`feature-${tabNumber}`);
+        tab.classList.add('active');
+        feature.classList.add('active');
+    });
+});
+
+// JavaScript for the project slider
+const projectSlider = document.querySelector('.project-slider');
+const projects = document.querySelectorAll('.project');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+let projectIndex = 0;
+
+function showProject(index) {
+  projects.forEach((project, i) => {
+    project.style.transform = `translateX(-${100 * (i - index)}%)`;
+  });
 }
+
+prevBtn.addEventListener('click', () => {
+  projectIndex = Math.max(0, projectIndex - 1);
+  showProject(projectIndex);
+});
+
+nextBtn.addEventListener('click', () => {
+  projectIndex = Math.min(projects.length - 1, projectIndex + 1);
+  showProject(projectIndex);
+});
+
+// Show the first project initially
+showProject(projectIndex);
